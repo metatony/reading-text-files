d612cc428d84a0f8856c6f3a5e4a41fc8617aec6
metatony <98693285+metatony@users.noreply.github.com>
2022-05-28 17:44:55 +0100

    Add files via upload

---
+++ readingtext.py
@@ -0,0 +1,22 @@
+def read_file_content(filename):
+    file_content = open("story.txt", "r")
+    data = file_content.read()
+    return data
+
+
+print(read_file_content("story.txt"))
+
+
+def count_words():
+    text = read_file_content("story.txt")
+    count = dict()
+    words = text.split()
+    for word in words:
+        if word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+    return count
+
+
+print(count_words())
